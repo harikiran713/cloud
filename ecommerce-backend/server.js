e4c6bd7dd69c88aa36885b782f3940f478c50ce7
harikiran713 <harikiranl713@gmail.com>
2025-04-07 11:44:10 +0530

    change

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -4,9 +4,7 @@ import 'dotenv/config.js'
 import connectDB from './config/mongodb.js'
 import {connectCloudinary} from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
-import productRouter from './routes/productRoute.js'
-import cartRouter from './routes/cartRoute.js'
-import orderRouter from './routes/orderRoute.js'
+
 
 // App Config
 const app = express()
